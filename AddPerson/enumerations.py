68361ad862724c60d82d4ea9a71d5cb20c580656
Kyle Bridburg <kbridbur@mit.edu>
2017-01-14 17:12:54 -0500

    Reworks class structure

--- AddPerson/enumerations.py
+++ AddPerson/enumerations.py
@@ -1,16 +1,19 @@
 POSSIBLE_PACKAGES = (
+    ('N', "None"),
     ('1',"package1"),
     ('2',"package2"),
     ('3',"package3")
 )
 
 SPECIALTIES = (
+    ('N', "None"),
     ('1', "specialty1"),
     ('2',"specialty2"),
     ('3',"specialty3")
 )
 
 SCHOOLS = (
+    ('N', "None"),
     ('HBS', 'Harvard Business School'),
     ('MIT', 'Massachusetts Institute of Techonology')
 )
--- AddPerson/models.py
+++ AddPerson/models.py
@@ -1,39 +1,46 @@
 from django.db import models
+from datetime import datetime
 import AddPerson.enumerations as enums
 # Create your models here.
-
-'''
-Represents an application.
-'''
-class Application(models.Model):
-    application_round = models.IntegerField()
-    application_school = models.CharField(max_length = 10, choices = enums.SCHOOLS)
-    application_success = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.application_school
-
 '''
 Represents a consultant as an object containing all of their personal information.
 Contains no information about relationship with clients or packages
 '''
 class Consultant(models.Model):
-    consultant_first_name = models.CharField(max_length = 100)
-    consultant_last_name = models.CharField(max_length = 100)
+    consultant_first_name = models.CharField(max_length = 100, blank = True)
+    consultant_last_name = models.CharField(max_length = 100, blank = True)
     consultant_specialty = models.CharField(max_length = 50, choices=enums.SPECIALTIES)
-    consultant_address = models.CharField(max_length = 200)
+    consultant_address = models.CharField(max_length = 200, blank = True)
 
     def __str__(self):
         return self.consultant_first_name + " " + self.consultant_last_name
+
+'''
+Represents a client who may have a consultant and one or more packages
+'''
+class Client(models.Model):
+    client_first_name = models.CharField(max_length = 100)
+    client_last_name = models.CharField(max_length = 100)
+    client_consultant = models.ForeignKey(Consultant, on_delete = models.PROTECT)
+    #other info
+
+    #params: new_package to add to the client
+    def AddPackage(self, new_package):
+        self.client_packages.add(new_package)
+
+    #returns client name as a string
+    def __str__(self):
+        return self.client_first_name + " " + self.client_last_name
 
 '''
 Represents a package as a package name and dates during which package is active/paid for
 Contains no information about which clients have this package
 '''
 class Package(models.Model):
+    client = models.ForeignKey(Client, null = True, blank = True)
     package_name = models.CharField(max_length = 1, choices=enums.POSSIBLE_PACKAGES)
-    package_start_date = models.DateField()
-    package_end_date = models.DateField()
+    package_start_date = models.DateField(default = datetime.now, blank = True)
+    package_end_date = models.DateField(default = datetime.now, blank = True)
 
     #params: new_end_date, a datetime object representing the new end date of the package
     def ChangeEndDate(self, new_end_date):
@@ -44,20 +51,13 @@ class Package(models.Model):
         return self.package_name
 
 '''
-Represents a client who may have a consultant and one or more packages
+Represents an application.
 '''
-class Client(models.Model):
-    client_first_name = models.CharField(max_length = 100)
-    client_last_name = models.CharField(max_length = 100)
-    client_packages = models.ManyToManyField(Package)
-    client_consultant = models.ForeignKey(Consultant, on_delete = models.CASCADE)
-    client_application = models.ManyToManyField(Application)
-    #other info
+class Application(models.Model):
+    client = models.ForeignKey(Client, null = True, blank = True)
+    application_round = models.IntegerField(default = 1, blank = True)
+    application_school = models.CharField(max_length = 10, choices = enums.SCHOOLS)
+    application_success = models.BooleanField(default=False, blank = True)
 
-    #params: new_package to add to the client
-    def AddPackage(self, new_package):
-        self.client_packages.add(new_package)
-
-    #returns client name as a string
     def __str__(self):
-        return self.client_first_name + " " + self.client_last_name
+        return self.application_school
