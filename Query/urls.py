19284ef7d981f28580d011b2f7782ebcc66446fe
Kyle Bridburg <kbridbur@mit.edu>
2017-02-13 16:10:26 -0500

    Rework search

--- Query/urls.py
+++ Query/urls.py
@@ -4,8 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='search'),
-    url(r'^search clients', views.client_search, name='search'),
-    url(r'^search consultants', views.consultant_search, name='search'),
-    url(r'^search providers', views.provider_search, name='search'),
-    url(r'^search editors', views.editor_search, name='search'),
 ]
--- Query/views.py
+++ Query/views.py
@@ -4,65 +4,42 @@ from AddPerson.models import Client, Consultant, Provider, Editor
 
 def index(request):
     urls = ['Client', 'Consultant', 'Editor', 'Provider']
-    if 'Client' in request.POST:
-        return HttpResponseRedirect('/search/search clients/')
-    elif 'Consultant' in request.POST:
-        return HttpResponseRedirect('/search/search consultants/')
-    elif 'Editor' in request.POST:
-        return HttpResponseRedirect('/search/search editors/')
-    elif 'Provider' in request.POST:
-        return HttpResponseRedirect('/search/search providers/')
-    return render(request, 'AddPerson/index_page.html', {'urls':urls, 'action':'search'})
-
-# Create your views here.
-def client_search(request):
     if request.method == 'GET': # If the form is submitted
 
-        search_query = request.GET.get('search_box', None)
-        if (search_query != None):
-            client_set = Client.FindClientsByName(search_query)
-            if search_query == "":
+        #Get the queries from each of the possible search boxes
+        consultant_query = request.GET.get('consultant_search', None)
+        provider_query = request.GET.get('provider_search', None)
+        editor_query = request.GET.get('editor_search', None)
+        client_query = request.GET.get('client_search', None)
+
+        #check which was queried and search appropriately
+        if (client_query != None):
+            client_set = Client.FindClientsByName(client_query)
+            if client_query == "":
                 query = "All Clients"
             else:
-                query = "clients by name of " + search_query
+                query = "clients by name of " + client_query
             return render(request, 'AddPerson/search_result.html', {'person_set':client_set, 'query': query})
-    return render(request, 'AddPerson/search.html')
-
-def consultant_search(request):
-    if request.method == 'GET': # If the form is submitted
-
-        search_query = request.GET.get('search_box', None)
-        if (search_query != None):
-            consultant_set = Consultant.FindConsultantsByName(search_query)
-            if search_query == "":
+        elif (consultant_query != None):
+            consultant_set = Consultant.FindConsultantsByName(consultant_query)
+            if consultant_query == "":
                 query = "All Consultants"
             else:
-                query = "consultants by name of " + search_query
+                query = "consultants by name of " + consultant_query
             return render(request, 'AddPerson/search_result.html', {'person_set':consultant_set, 'query': query})
-    return render(request, 'AddPerson/search.html')
-
-def provider_search(request):
-    if request.method == 'GET': # If the form is submitted
-
-        search_query = request.GET.get('search_box', None)
-        if (search_query != None):
-            provider_set = Provider.FindProvidersByName(search_query)
-            if search_query == "":
+        elif (provider_query != None):
+            provider_set = Provider.FindProvidersByName(provider_query)
+            if provider_query == "":
                 query = "All Providers"
             else:
-                query = "providers by name of " + search_query
+                query = "providers by name of " + provider_query
             return render(request, 'AddPerson/search_result.html', {'person_set':provider_set, 'query': query})
-    return render(request, 'AddPerson/search.html')
-
-def editor_search(request):
-    if request.method == 'GET': # If the form is submitted
-
-        search_query = request.GET.get('search_box', None)
-        if (search_query != None):
-            editor_set = Editor.FindEditorsByName(search_query)
-            if search_query == "":
+        elif (editor_query != None):
+            editor_set = Editor.FindEditorsByName(editor_query)
+            if editor_query == "":
                 query = "All Editors"
             else:
-                query = "editors by name of " + search_query
+                query = "editors by name of " + editor_query
             return render(request, 'AddPerson/search_result.html', {'person_set':editor_set, 'query': query})
-    return render(request, 'AddPerson/search.html')
+
+    return render(request, 'AddPerson/index_page.html', {'urls':urls, 'action':'search'})
